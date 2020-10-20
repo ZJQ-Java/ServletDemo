02a2392ce9176a19f29b5465c41f8cc1b2c5a522
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-10-20 14:41:47 +0800

    error

--- ServletParent/ServletDemo1/src/main/java/com/qiu/servlet/GetServlet.java
+++ ServletParent/ServletDemo1/src/main/java/com/qiu/servlet/GetServlet.java
@@ -6,13 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
 
 public class GetServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext servletContext = this.getServletContext();
-        String userName = (String) servletContext.getAttribute("userName");
-        System.out.println(userName);
+        String value = (String) servletContext.getAttribute("userName");
+        value = Optional.ofNullable(value).orElse(servletContext.getInitParameter("url"));
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + value + "<h1>");
 
     }
 
