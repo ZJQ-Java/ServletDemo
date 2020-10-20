bf01b9e29fdf5166b8cd59c33f370a9c11b5c231
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-10-20 12:02:25 +0800

    servletDemo

---
+++ ServletParent/ServletDemo1/src/main/java/com/qiu/servlet/HelloServlet/helloServlet.java
@@ -0,0 +1,33 @@
+package com.qiu.servlet.HelloServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class helloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>你好牙!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        ServletContext servletContext = this.getServletContext();
+        servletContext.setAttribute("userName","zjq");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
